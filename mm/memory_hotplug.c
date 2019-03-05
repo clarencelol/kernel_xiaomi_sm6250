d6d35fb489492a5265cb1b97d282bd12809f0468
Wei Yang <richard.weiyang@gmail.com>
2019-03-05 15:44:01 -0800

    mm: remove extra drain pages on pcp list
    In the current implementation, there are two places to isolate a range
    of page: __offline_pages() and alloc_contig_range().  During this
    procedure, it will drain pages on pcp list.
    
    Below is a brief call flow:
    
      __offline_pages()/alloc_contig_range()
          start_isolate_page_range()
              set_migratetype_isolate()
                  drain_all_pages()
          drain_all_pages()                 <--- A
    
    This snippet shows the current logic is isolate and drain pcp list for
    each pageblock and drain pcp list again for the whole range.
    
    start_isolate_page_range is responsible for isolating the given pfn
    range.  One part of that job is to make sure that also pages that are on
    the allocator pcp lists are properly isolated.  Otherwise they could be
    reused and the range wouldn't be completely isolated until the memory is
    freed back.  While there is no strict guarantee here because pages might
    get allocated at any time before drain_all_pages is called there doesn't
    seem to be any strong demand for such a guarantee.
    
    In any case, draining is already done at the isolation level and there
    is no need to do it again later by start_isolate_page_range callers
    (memory hotplug and CMA allocator currently).  Therefore remove
    pointless draining in existing callers to make the code more clear and
    functionally correct.
    
    [mhocko@suse.com: provide a clearer changelog for the last two paragraphs]
    Link: http://lkml.kernel.org/r/20190105233141.2329-1-richard.weiyang@gmail.com
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1719,7 +1719,6 @@ repeat:
 	if (drain) {
 		lru_add_drain_all_cpuslocked();
 		cond_resched();
-		drain_all_pages(zone);
 	}
 
 	pfn = scan_movable_pages(start_pfn, end_pfn);
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7982,7 +7982,6 @@ int alloc_contig_range(unsigned long start, unsigned long end,
 	 */
 
 	lru_add_drain_all();
-	drain_all_pages(cc.zone);
 
 	order = 0;
 	outer_start = start;
