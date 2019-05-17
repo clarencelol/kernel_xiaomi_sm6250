cd481c53b56ace6da201e7731de5673dafb77d8b
Chenxi Mao <chenxi.mao@sony.com>
2019-05-17 13:56:24 +0800

    LZ4: Port LZ4 1.9.x FAST_DEC_LOOP and enable it on x86 and ARM64
    FAST_DEC_LOOP was introduced from LZ4 1.9.0[1]
    This change would be introduce 10% on decompress operation
    according to LZ4 benchmark result on X86 devices.
    Meanwhile, LZ4 with FAST_DEC_LOOP could get improvements on ARM64,
    however clang compiler has downgrade if FAST_DEC_LOOP enabled.
    
    So FAST_DEC_LOOP only enabled on X86/X86-64 or ARM64 with GCC build.
    
    LZ4 FAST_DEC_LOOP bug fixes include as well.
    1. fixed read-after input in LZ4_decompress_safe() (issue 681)
    2. Fix out-of-bound read in LZ4_decompress_fast() (issue 676)
    
    PS2:
    1. Move common API to lz4defs.h
    2. Add PPC related inline Macro defination.
    3. Force inline new static apis.
    
    Here is the test result on ARM64(cortex-A53)
    Benchmark via ZRAM:
    
    Test case:
    taskset 03 /data/fio --bs=32k --randrepeat=1 --randseed=100 --refill_buffers \
    --buffer_compress_percentage=75  --size=700M \
    --scramble_buffers=1 --direct=1 --loops=100 --numjobs=1 \
    --filename=/data/test/test --name=seq-read --rw=read --stonewall
    
    Patched:
        READ: bw=150MiB/s (157MB/s)
    Vanilla:
        READ: bw=135MiB/s (142MB/s)
    
    [1] https://github.com/lz4/lz4/releases/tag/v1.9.0
    
    Signed-off-by: chenxi.mao <chenxi.mao@sony.com>
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- lib/lz4/lz4_compress.c
+++ lib/lz4/lz4_compress.c
@@ -306,7 +306,7 @@ static FORCE_INLINE int LZ4_compress_generic(
 				*token = (BYTE)(litLength << ML_BITS);
 
 			/* Copy Literals */
-			LZ4_wildCopy(op, anchor, op + litLength);
+			LZ4_wildCopy8(op, anchor, op + litLength);
 			op += litLength;
 		}
 
