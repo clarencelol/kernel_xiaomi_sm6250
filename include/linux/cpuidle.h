37a8b1f80c933f89451d93d1741637a1fda6c857
Marcelo Tosatti <mtosatti@redhat.com>
2019-07-03 20:51:27 -0300

    FIXUP: governors: unify last_state_idx
    Since this field is shared by all governors, move it to
    cpuidle device structure.
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -79,6 +79,7 @@ struct cpuidle_device {
 	unsigned int		use_deepest_state:1;
 	unsigned int		cpu;
 
+	int			last_state_idx;
 	int			last_residency;
 	struct cpuidle_state_usage	states_usage[CPUIDLE_STATE_MAX];
 	struct cpuidle_state_kobj *kobjs[CPUIDLE_STATE_MAX];
