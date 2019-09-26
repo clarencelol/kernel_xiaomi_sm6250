2765f84ca6ad180d86f2d024f8929192668f5a31
Kevin(Yudong) Yang <yyd@google.com>
2019-09-26 10:30:05 -0400

    BACKPORT: tcp_bbr: fix quantization code to not raise cwnd if not probing bandwidth
    There was a bug in the previous logic that attempted to ensure gain cycling
    gets inflight above BDP even for small BDPs. This code correctly raised and
    lowered target inflight values during the gain cycle. And this code
    correctly ensured that cwnd was raised when probing bandwidth. However, it
    did not correspondingly ensure that cwnd was *not* raised in this way when
    *not* probing for bandwidth. The result was that small-BDP flows that were
    always cwnd-bound could go for many cycles with a fixed cwnd, and not probe
    or yield bandwidth at all. This meant that multiple small-BDP flows could
    fail to converge in their bandwidth allocations.
    
    Fixes: 3c346b233c68 ("tcp_bbr: fix bw probing to raise in-flight data for very small BDPs")
    Signed-off-by: Kevin(Yudong) Yang <yyd@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Priyaranjan Jha <priyarjha@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -357,7 +357,7 @@ static u32 bbr_bdp(struct sock *sk, u32 bw, int gain)
  * which allows 2 outstanding 2-packet sequences, to try to keep pipe
  * full even with ACK-every-other-packet delayed ACKs.
  */
-static u32 bbr_quantization_budget(struct sock *sk, u32 cwnd, int gain)
+static u32 bbr_quantization_budget(struct sock *sk, u32 cwnd)
 {
 	struct bbr *bbr = inet_csk_ca(sk);
 
@@ -368,7 +368,7 @@ static u32 bbr_quantization_budget(struct sock *sk, u32 cwnd, int gain)
 	cwnd = (cwnd + 1) & ~1U;
 
 	/* Ensure gain cycling gets inflight above BDP even for small BDPs. */
-	if (bbr->mode == BBR_PROBE_BW && gain > BBR_UNIT)
+	if (bbr->mode == BBR_PROBE_BW && bbr->cycle_idx == 0)
 		cwnd += 2;
 
 	return cwnd;
@@ -380,7 +380,7 @@ static u32 bbr_inflight(struct sock *sk, u32 bw, int gain)
 	u32 inflight;
 
 	inflight = bbr_bdp(sk, bw, gain);
-	inflight = bbr_quantization_budget(sk, inflight, gain);
+	inflight = bbr_quantization_budget(sk, inflight);
 
 	return inflight;
 }
@@ -447,7 +447,7 @@ static void bbr_set_cwnd(struct sock *sk, const struct rate_sample *rs,
 
 	/* If we're below target cwnd, slow start cwnd toward target cwnd. */
 	target_cwnd = bbr_bdp(sk, bw, gain);
-	target_cwnd = bbr_quantization_budget(sk, target_cwnd, gain);
+	target_cwnd = bbr_quantization_budget(sk, target_cwnd);
 	if (bbr_full_bw_reached(sk))  /* only cut cwnd if we filled the pipe */
 		cwnd = min(cwnd + acked, target_cwnd);
 	else if (cwnd < target_cwnd || tp->delivered < TCP_INIT_CWND)
