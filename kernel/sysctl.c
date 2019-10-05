eff149149e0acdaf3863c3aed32b05bb566901bc
Rick Yiu <rickyiu@google.com>
2019-10-05 15:59:06 +0800

    sched: use sysctl to control energy aware feature
    Energy aware feature control is previously done through debugfs,
    which will be deprecated, so move the control to sysctl.
    
    Bug: 141333728
    Test: function works as expected
    Change-Id: I55411d3bb2669ba1fae3225d67cdf1cf8b3b3a7f
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -472,6 +472,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_updown_migrate_handler,
 	},
 	{
+		.procname       = "sched_energy_aware",
+		.data           = &sysctl_sched_energy_aware,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1         = &zero,
+		.extra2         = &one,
+	},
+	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),
