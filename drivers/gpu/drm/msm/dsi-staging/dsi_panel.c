e6c6ee81ae083d4b6b3f979f9257cf186ac9cf14
Demon000 <demonsingur@gmail.com>
2020-07-22 00:38:46 +0300

    dsi-staging: panel: let touchscreen panels keep display reset high
    The touchscreen probably shares the reset pin with the display,
    so when the display is turned off and the touchscreen is in gesture
    mode, the display shouldn't pull down the reset pin.
    
    Change-Id: I2b9b52ede750001d57c5c0e56e32ef29763cf5b0

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -471,6 +471,13 @@ exit:
 	return rc;
 }
 
+static bool lcd_reset_keep_high;
+void set_lcd_reset_gpio_keep_high(bool en)
+{
+	lcd_reset_keep_high = en;
+}
+EXPORT_SYMBOL(set_lcd_reset_gpio_keep_high);
+
 static int dsi_panel_power_off(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -479,7 +486,8 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
 	if (gpio_is_valid(panel->reset_config.reset_gpio))
-		gpio_set_value(panel->reset_config.reset_gpio, 0);
+		if (!lcd_reset_keep_high)
+			gpio_set_value(panel->reset_config.reset_gpio, 0);
 
 	if (gpio_is_valid(panel->reset_config.lcd_mode_sel_gpio))
 		gpio_set_value(panel->reset_config.lcd_mode_sel_gpio, 0);
