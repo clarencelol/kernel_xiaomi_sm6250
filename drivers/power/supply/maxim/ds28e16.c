1fcdff23adf0138e5e8b8fd116d178189cb4e00d
Demon000 <demonsingur@gmail.com>
2020-08-12 21:01:00 +0300

    ds28e16: Remove unused props
    Change-Id: I940880101ac8aacbd161f0d00c813ae12ce7c99b

--- drivers/power/supply/maxim/ds28e16.c
+++ drivers/power/supply/maxim/ds28e16.c
@@ -1274,9 +1274,6 @@ static enum power_supply_property verify_props[] = {
 	POWER_SUPPLY_PROP_PAGENUMBER,
 	POWER_SUPPLY_PROP_PAGEDATA,
 	POWER_SUPPLY_PROP_AUTHEN_RESULT,
-	POWER_SUPPLY_PROP_SESSION_SEED,
-	POWER_SUPPLY_PROP_S_SECRET,
-	POWER_SUPPLY_PROP_CHALLENGE,
 	POWER_SUPPLY_PROP_AUTH_ANON,
 	POWER_SUPPLY_PROP_AUTH_BDCONST,
 	POWER_SUPPLY_PROP_PAGE0_DATA,
@@ -1361,30 +1358,10 @@ static int verify_set_property(struct power_supply *psy,
 			       enum power_supply_property prop,
 			       const union power_supply_propval *val)
 {
-	//int ret;
-	//unsigned char buf[50];
-
 	switch (prop) {
 	case POWER_SUPPLY_PROP_PAGENUMBER:
 		pagenumber = val->intval;
 		break;
-/*
-	case POWER_SUPPLY_PROP_PAGEDATA:
-		memcpy(buf, val->arrayval, 16);
-		ret = DS28E16_cmd_writeMemory(pagenumber, buf);
-		if (ret != DS_TRUE)
-			return -EAGAIN;
-		break;
-	case POWER_SUPPLY_PROP_SESSION_SEED:
-		memcpy(session_seed, val->arrayval, 32);
-		break;
-	case POWER_SUPPLY_PROP_S_SECRET:
-		memcpy(S_secret, val->arrayval, 32);
-		break;
-	case POWER_SUPPLY_PROP_CHALLENGE:
-		memcpy(challenge, val->arrayval, 32);
-		break;
-*/
 	case POWER_SUPPLY_PROP_AUTH_ANON:
 		auth_ANON  = val->intval;
 		break;
@@ -1406,10 +1383,6 @@ static int verify_prop_is_writeable(struct power_supply *psy,
 
 	switch (prop) {
 	case POWER_SUPPLY_PROP_PAGENUMBER:
-	case POWER_SUPPLY_PROP_PAGEDATA:
-	case POWER_SUPPLY_PROP_SESSION_SEED:
-	case POWER_SUPPLY_PROP_S_SECRET:
-	case POWER_SUPPLY_PROP_CHALLENGE:
 	case POWER_SUPPLY_PROP_AUTH_ANON:
 	case POWER_SUPPLY_PROP_AUTH_BDCONST:
 		ret = 1;
