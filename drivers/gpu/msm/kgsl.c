a8a915e25205de0c844f71341c0cfb43a07b46a3
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-21 14:51:20 +0200

    msm: kgsl: Affine IRQ and worker kthread to the big CPU cluster
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ia71efe1409e10cccbdb2157ea91f576137651d70

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5258,7 +5258,7 @@ static int __init kgsl_core_init(void)
 
 	kthread_init_worker(&kgsl_driver.worker);
 
-	kgsl_driver.worker_thread = kthread_run(kthread_worker_fn,
+	kgsl_driver.worker_thread = kthread_run_perf_critical(cpu_perf_mask, kthread_worker_fn,
 		&kgsl_driver.worker, "kgsl_worker_thread");
 
 	kthread_init_worker(&kgsl_driver.low_prio_worker);
