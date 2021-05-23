3d0395a32df7b81cd8ac5c01e4b3f1aade8a9965
Yaroslav Furman <yaro330@gmail.com>
2021-05-24 04:08:14 +0530

    kernel: printk: silence logd spam
    Sometimes userspace floods logs with messages like this:
    [38175.285310] logd: Skipping 256 entries from slow reader, pid 9514, from LogBuffer::kickMe()
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -807,7 +807,6 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 		if (memcmp(line+3, "batteryd", sizeof("batteryd")-1) == 0 ||
 			   memcmp(line+3, "healthd", sizeof("healthd")-1) == 0)
 			goto ignore;
-		{
 		char *endp = NULL;
 		unsigned int u;
 
@@ -819,7 +818,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
-		}
+			if(strncmp(line, "logd: Skipping", sizeof("logd: Skipping")))
+				goto ignore;
 		}
 	}
 
