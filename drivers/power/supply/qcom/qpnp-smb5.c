e25028bef802bde253128c126d1278fa569a6dec
Tcheral <lcockx@protonmail.com>
2021-07-28 11:23:08 +0530

    drivers: power: Fix charging indicator and notification leds
    * notes from owner extracted from mojito xiaomi change  - contain only change needed for fast charge indication ( in all miatoll as well ) - used over positron source
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1739,7 +1739,9 @@ static int smb5_usb_main_set_prop(struct power_supply *psy,
 		vote_override(chg->usb_icl_votable, CC_MODE_VOTER,
 				(val->intval < 0)||( chg->cc_un_compliant_detected == true ) ? false : true, val->intval);
 		/* Main ICL updated re-calculate ILIM */
-		rerun_election(chg->fcc_votable);
+		if (real_chg_type == POWER_SUPPLY_TYPE_USB_HVDCP_3 ||
+			real_chg_type == POWER_SUPPLY_TYPE_USB_HVDCP_3P5)
+			rerun_election(chg->fcc_votable);
 		break;
 	case POWER_SUPPLY_PROP_COMP_CLAMP_LEVEL:
 		rc = smb5_set_prop_comp_clamp_level(chg, val);
