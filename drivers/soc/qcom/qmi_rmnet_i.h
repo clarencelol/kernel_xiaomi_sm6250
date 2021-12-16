cc718eb572f3c5f0c9fe9c7900678f7d7c03c0a1
Sharath Chandra Vurukala <quic_sharathv@quicinc.com>
2021-12-16 11:00:03 +0530

    dfc: reset tx queue
    When a bearer is removed, calling qdisc reset on a tx queue
    could have a race condition with qdisc dequeue for lockless
    qdisc such as pfifo_fast.
    
    This change uses a different mechanism not relying on qdisc
    implementation to achieve packet purge on bearer remove.
    
    Change-Id: I8f9201809853b07293896d6cb8e010e9e0904e46
    Signed-off-by: Weiyi Chen <quic_weiyic@quicinc.com>
    Signed-off-by: Sharath Chandra Vurukala <quic_sharathv@quicinc.com>

--- drivers/soc/qcom/qmi_rmnet_i.h
+++ drivers/soc/qcom/qmi_rmnet_i.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -79,6 +80,7 @@ struct svc_info {
 
 struct mq_map {
 	struct rmnet_bearer_map *bearer;
+	bool drop_on_remove;
 };
 
 struct qos_info {
