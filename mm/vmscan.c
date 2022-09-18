c53301b7ff0e9a7575aac2814efcc495a7690858
Yu Zhao <yuzhao@google.com>
2022-09-18 14:47:51 -0600

    mm: multi-gen LRU: dial down MAX_LRU_BATCH
    A need_resched warning was reported and already fixed by adding a
    need_resched() check to walk_pud_range(). Dial down MAX_LRU_BATCH
    anyway in the interest of direct reclaim latency.
    
    WARNING: CPU: 22 PID: 2771 at kernel/sched/core.c:3637 scheduler_tick+0x339/0x410
    Call Trace:
     <IRQ>
     update_process_times+0x7b/0x90
     tick_sched_timer+0x82/0xd0
     __run_hrtimer+0x81/0x200
     hrtimer_interrupt+0x192/0x450
     smp_apic_timer_interrupt+0xac/0x1d0
     apic_timer_interrupt+0x88/0x90
     </IRQ>
    RIP: 0010:walk_pte_range+0x1c2/0x6a0
     walk_pmd_range+0x1ed/0x490
     walk_pud_range+0xe6/0x2b0
     __walk_page_range+0x111/0x690
     walk_page_range+0x4e/0x150
     walk_mm+0x110/0x200
     try_to_inc_max_seq+0xdb/0xb10
     lru_gen_run_cmd+0x153/0x1c0
     lru_gen_run+0x150/0x210
     stale_page_run+0x62/0x730
     kthread+0x148/0x1b0
     ret_from_fork+0x54/0x60
    
    Reported-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2499,7 +2499,7 @@ out:
  */
 #define MIN_NR_GENS	2
 
-#define MAX_BATCH_SIZE	8192
+#define MAX_BATCH_SIZE	4096
 
 /******************************************************************************
  *                          shorthand helpers
@@ -3379,7 +3379,7 @@ restart:
 
 		walk_pmd_range(&val, addr, next, walk);
 
-		if (args->batch_size >= MAX_BATCH_SIZE) {
+		if (need_resched() || args->batch_size >= MAX_BATCH_SIZE) {
 			end = (addr | ~PUD_MASK) + 1;
 			goto done;
 		}
@@ -3638,7 +3638,7 @@ static bool walk_mm_list(struct lruvec *lruvec, unsigned long max_seq,
 
 	if (!last) {
 		/* the foreground aging prefers not to wait */
-		if (!current_is_kswapd() && sc->priority < DEF_PRIORITY - 2)
+		if (!current_is_kswapd() && sc->priority <= DEF_PRIORITY - 2)
 			wait_event_killable(mm_list->nodes[nid].wait,
 					    max_seq < READ_ONCE(lrugen->max_seq));
 
