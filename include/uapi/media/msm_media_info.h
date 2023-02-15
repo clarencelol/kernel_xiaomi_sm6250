52f69029327efb00edc38cdfe469c8564e460b8c
Giovanni Ricca <giovanniricca@proton.me>
2023-02-15 23:17:37 +0800

    Revert "uapi/media: Update yuv buffer size based on hardware requirement"
    Re-Enable extra data to avoid venus errors when
    playing vp8/vp9 files.
    
    With enabled debug there's some kernel log which points
    to the error.
    I msm_vidc: dbg: message queue is empty, rx_req = 1, tx_req = 0, read_idx = 47183
    I msm_vidc: fw:
    I <VFW_E  : HostDr:vp9d:8cac11c0:0> vDec_SessionSetProperty(2308): HostDriver: vDec_SessionSetProperty, Actual buffer size is less than default for Output
    
    And also user space complains:
    E OMX-VDEC-1080P: Error: Insufficient size allocated for extra-data
    
    Test: atest android.mediav2.cts.EncodeDecodeAccuracyTest#testEncodeDecodeAccuracyRGB
    
    This reverts commit 722168b5f8c8b9f86498d29121aa6944ed1b760b.
    
    Co-Authored-By: Quallenauge <Hamsi2k@freenet.de>
    Change-Id: I2961f02ba6c56001a6798c35ce12267f0645a462
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- include/uapi/media/msm_media_info.h
+++ include/uapi/media/msm_media_info.h
@@ -1333,6 +1333,7 @@ invalid_input:
 static inline unsigned int VENUS_BUFFER_SIZE(
 	int color_fmt, int width, int height)
 {
+	const unsigned int extra_size = VENUS_EXTRADATA_SIZE(width, height);
 	unsigned int uv_alignment = 0, size = 0;
 	unsigned int y_plane, uv_plane, y_stride,
 		uv_stride, y_sclines, uv_sclines;
@@ -1362,7 +1363,8 @@ static inline unsigned int VENUS_BUFFER_SIZE(
 		uv_alignment = 4096;
 		y_plane = y_stride * y_sclines;
 		uv_plane = uv_stride * uv_sclines + uv_alignment;
-		size = y_plane + uv_plane;
+		size = y_plane + uv_plane +
+				MSM_MEDIA_MAX(extra_size, 8 * y_stride);
 		size = MSM_MEDIA_ALIGN(size, 4096);
 		break;
 	case COLOR_FMT_NV12_MVTB:
@@ -1370,7 +1372,7 @@ static inline unsigned int VENUS_BUFFER_SIZE(
 		y_plane = y_stride * y_sclines;
 		uv_plane = uv_stride * uv_sclines + uv_alignment;
 		size = y_plane + uv_plane;
-		size = 2 * size;
+		size = 2 * size + extra_size;
 		size = MSM_MEDIA_ALIGN(size, 4096);
 		break;
 	case COLOR_FMT_NV12_UBWC:
@@ -1390,7 +1392,8 @@ static inline unsigned int VENUS_BUFFER_SIZE(
 			uv_meta_scanlines, 4096);
 
 		size = (y_ubwc_plane + uv_ubwc_plane + y_meta_plane +
-			uv_meta_plane)*2;
+			uv_meta_plane)*2 +
+			MSM_MEDIA_MAX(extra_size + 8192, 48 * y_stride);
 		size = MSM_MEDIA_ALIGN(size, 4096);
 		break;
 	case COLOR_FMT_NV12_BPP10_UBWC:
@@ -1406,7 +1409,8 @@ static inline unsigned int VENUS_BUFFER_SIZE(
 					uv_meta_scanlines, 4096);
 
 		size = y_ubwc_plane + uv_ubwc_plane + y_meta_plane +
-			uv_meta_plane;
+			uv_meta_plane +
+			MSM_MEDIA_MAX(extra_size + 8192, 48 * y_stride);
 		size = MSM_MEDIA_ALIGN(size, 4096);
 		break;
 	case COLOR_FMT_P010_UBWC:
