2c9cd90e6be9e12f4ba01a15bfe99c0e19b388b4
onettboots <blackcocopet@gmail.com>
2023-04-13 00:26:27 +0000

    fs: add kernelsu hook
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1704,6 +1704,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1717,6 +1720,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
