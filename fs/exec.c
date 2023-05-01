e3252be4af25232d29f387bb853a5f03dba674c7
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-05-01 17:01:51 +0800

    treewide: Guard KernelSU changes
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1703,8 +1703,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1719,9 +1721,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
+#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-
+#endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
