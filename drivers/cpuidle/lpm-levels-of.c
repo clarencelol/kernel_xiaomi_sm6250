Kaz205/renoir

lpm-levels: Parse all DT idle values
This is required for other governors to work.

Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>

drivers/cpuidle/lpm-levels-of.c
@@ -399,6 +399,8 @@ static int parse_power_params(struct device_node *dn, struct power_params *pwr)
 	ret = lpm_of_read_u32(dn, "min-residency-us",
 			      &pwr->min_residency, true);
 
+	pwr->local_timer_stop = of_property_read_bool(dn, "local-timer-stop");
+
 	return ret;
 }
 

clarencelol/kernel_xiaomi_sm6250

drivers/cpuidle/lpm-levels-of.c
@@ -396,6 +396,8 @@ static int parse_power_params(struct device_node *node,
 fail:
 	pr_err("Failed to read key: %s node: %s\n", key, node->name);
 
+	pwr->local_timer_stop = of_property_read_bool(node, "local-timer-stop");
+
 	return ret;
 }
 